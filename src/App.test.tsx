bc99e93e290f6e77aa4f397cf020839159e5db02
thuan.ngo <thuan.ngo@eastagile.com>
2021-10-09 00:14:01 +0700

    Init eslint, prettier and tailwindcss

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByText(/This is a react app/i)).toBeInTheDocument();
 });
